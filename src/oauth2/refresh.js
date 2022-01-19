21ddef915448cf6eda31d0e3126b40745c08a04b
jangji96 <xxwlgns@naver.com>
2022-01-19 13:43:28 +0900

    log 추가

--- src/oauth2/refresh.js
+++ src/oauth2/refresh.js
@@ -2,11 +2,13 @@ import Cookie from "js-cookie";
 import moment from "moment";
 import OAuth2RedirectHandler from "./OAuth2RedirectHandler";
 const refresh = async (config) => {
+  console.log("refresh 실행 테스트");
   let { accessToken, expireAT } = JSON.parse(
     localStorage.getItem("accessToken")
   );
 
   if (moment(expireAT).diff(moment())) {
+    console.log("accessToken 만료 토큰 재발급 요청");
     OAuth2RedirectHandler();
   }
   config.headers["Authorization"] = `Bearer ${accessToken}`;
