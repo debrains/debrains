ac083a8be04171dd92dd581472066a734ddfcd3f
jangji96 <xxwlgns@naver.com>
2022-01-19 12:49:52 +0900

    토큰 재발급 수정

--- src/oauth2/refresh.js
+++ src/oauth2/refresh.js
@@ -2,24 +2,14 @@ import axios from "axios";
 import Cookie from "js-cookie";
 import { API_BASE_URL } from "../contants/index";
 import moment from "moment";
+import OAuth2RedirectHandler from "./OAuth2RedirectHandler";
 const refresh = async (config) => {
-  const refreshToken = Cookie.get("refreshToken");
   let { accessToken, expireAT } = JSON.parse(
     localStorage.getItem("accessToken")
   );
 
-  if (moment(expireAT).diff(moment()) < 0 && refreshToken) {
-    const body = {
-      refreshToken,
-    };
-    // 토큰 갱신 서버통신
-    const { data } = await axios.post(`${API_BASE_URL}/auth/token`, body);
-    accessToken = data.data.accessToken;
-    localStorage.setItem("accessToken", data.data.accessToken);
-    localStorage.setItem(
-      "expiresAt",
-      moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss")
-    );
+  if (moment(expireAT).diff(moment())) {
+    OAuth2RedirectHandler();
   }
   config.headers["Authorization"] = `Bearer ${accessToken}`;
   return config;
