ae6245f15092b5cf532298570378c4927c20c95b
jangji96 <xxwlgns@naver.com>
2022-01-20 18:03:33 +0900

    access토큰 재발급

--- src/oauth2/refresh.js
+++ src/oauth2/refresh.js
@@ -1,4 +1,3 @@
-import Cookie from "js-cookie";
 import moment from "moment";
 import { ACCESS_TOKEN, API_BASE_URL } from "../contants";
 
@@ -8,16 +7,20 @@ const refresh = async (config) => {
     localStorage.getItem("accessToken")
   );
 
-  if (moment(expireAT).diff(moment()) < 9) {
+  if (moment(expireAT).diff(moment()) > 9) {
     console.log("accessToken 만료 토큰 재발급 요청");
     const newToken = await fetch("http://localhost:8080/auth/refresh", {
+      method: "POST",
       body: accessToken,
+      credentials: "include",
     });
     const object = {
       accessToken: newToken,
       expireAT: moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss"),
     };
-    console.log("newToken :", newToken);
+    newToken.text().then(function (text) {
+      console.log("text: ", text);
+    });
     if (newToken) {
       localStorage.setItem(ACCESS_TOKEN, JSON.stringify(object));
     }
