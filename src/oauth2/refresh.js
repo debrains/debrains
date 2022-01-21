7fa7fa97c477380dd9c8eccfcf91210a865c6943
jangji06 <xxwlgns@naver.com>
2022-01-21 09:31:06 +0900

    access 토큰 재발급 저장 / 프로필 불러오기

--- src/oauth2/refresh.js
+++ src/oauth2/refresh.js
@@ -14,16 +14,16 @@ const refresh = async (config) => {
       body: accessToken,
       credentials: "include",
     });
-    const object = {
-      accessToken: newToken,
-      expireAT: moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss"),
-    };
-    newToken.text().then(function (text) {
-      console.log("text: ", text);
+
+    await newToken.text().then(function (tokenText) {
+      const object = {
+        accessToken: tokenText,
+        expireAT: moment().add(1, "hour").format("yyyy-MM-DD HH:mm:ss"),
+      };
+      if (tokenText) {
+        localStorage.setItem(ACCESS_TOKEN, JSON.stringify(object));
+      }
     });
-    if (newToken) {
-      localStorage.setItem(ACCESS_TOKEN, JSON.stringify(object));
-    }
   }
   config.headers["Authorization"] = `Bearer ${accessToken}`;
   return config;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import { getCurrentUser } from "../apis/api";
 function Main() {
   const [user, setUser] = useState("로그인 전");
+  const [imgURL, setImgURL] = useState("");
 
   function onClick() {
     const data = getCurrentUser();
-    setUser(data);
+    setUser(data.name);
+    setImgURL(data.imageUrl);
   }
 
   return (
     <div>
       <div>{user}</div>
+      {imgURL !== "" ? <img src={imgURL} alt={user} /> : null}
       <button onClick={onClick}>테스트 버튼</button>
     </div>
   );
