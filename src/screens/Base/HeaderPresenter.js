8adde6b40485f187164b5f2db5bb159520cd853e
jangji06 <xxwlgns@naver.com>
2022-02-07 00:17:33 +0900

    header

--- src/screens/Base/HeaderPresenter.js
+++ src/screens/Base/HeaderPresenter.js
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   border-bottom: 1px solid violet;
+  align-items: center;
+  padding: 0.3rem;
 `;
 
 const Spacer = styled.div`
   flex-grow: 1;
 `;
 
-const Button = styled.button``;
+const BorderedButton = styled(Link)`
+  font-weight: 600;
+  color: violet;
+  border: 1px solid violet;
+  padding: 0.5rem;
+  cursor: pointer;
+  border-radius: 2px;
+  text-decoration: none;
+  transition: 0.2s all;
+
+  &:hover {
+    background: violet;
+    color: white;
+  }
+`;
 
 const Logo = styled.div`
   font-size: 2rem;
@@ -23,11 +40,7 @@ function HeaderPresenter({ children }) {
     <Wrapper>
       <Logo>Debrains</Logo>
       <Spacer />
-      <div>가운데</div>
-      <Spacer />
-      <div>가운데</div>
-      <Spacer />
-      <Button>로그인</Button>
+      <BorderedButton to="/myPage">로그인 / 회원가입</BorderedButton>
     </Wrapper>
   );
 }
