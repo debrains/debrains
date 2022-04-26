1bf2ff7460709e821aa1ea674ad4c37b9bd2b4a0
jangji96 <xxwlgns@naver.com>
2022-04-26 16:08:42 +0900

    서버별 api 요청 url 적용

--- src/contents/index.js
+++ src/contents/index.js
@@ -1,7 +1,22 @@
-export const API_BASE_URL = "http://localhost:8080";
+console.log(window.location.origin);
+let URL = "http://localhost:8080";
+let redirect = "http://localhost:3000/oauth2/redirect";
+
+if (window.location.origin === "https://debrain.co.kr") {
+  URL = "https://api.debrain.co.kr";
+} else {
+  URL = "http://3.39.86.48:8080";
+}
+export const API_BASE_URL = URL;
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+if (window.location.origin === "https://debrain.co.kr") {
+  redirect = "https://debrain.co.kr/oauth2/redirect";
+} else {
+  redirect = "http://3.39.86.48/oauth2/redirect";
+}
+
+export const OAUTH2_REDIRECT_URI = redirect;
 
 export const GOOGLE_AUTH_URL =
   API_BASE_URL +
