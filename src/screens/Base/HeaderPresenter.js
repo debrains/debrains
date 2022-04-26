1c7a3cb8db2dce15a65d88319d3cbd2acebb3d73
jangji96 <xxwlgns@naver.com>
2022-04-26 10:07:52 +0900

    Develop (#20)
    * Deb 108 til 목록 무한스크롤 (#17)
    
    * 로그인 시 로그인 버튼 숨기기
    
    * til 인증 ui 수정 / til 인증 생성 api 연동
    
    * 프로필 수정 api 연동
    
    * 인증 상세화면
    
    * til 목록 수정 / til 삭제
    
    Co-authored-by: SANGIL PARK <europani1212@gmail.com>
    
    * 로그인 임시수정
    
    Co-authored-by: SANGIL PARK <europani1212@gmail.com>

--- src/screens/Base/HeaderPresenter.js
+++ src/screens/Base/HeaderPresenter.js
@@ -192,6 +192,12 @@ export default function Example() {
                   마이페이지
                 </Link>
               )}
+              <Link
+                to="/login"
+                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-purple-700 hover:bg-purple-700"
+              >
+                로그인
+              </Link>
             </div>
           </div>
         </div>
