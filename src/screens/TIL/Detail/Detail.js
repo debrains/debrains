8c0d1ff6e84130e2f3da50c06b6e4ab4f9a74526
jangji96 <xxwlgns@naver.com>
2022-04-26 09:52:07 +0900

    Deb 108 til 목록 무한스크롤 (#17) (#18)
    * 로그인 시 로그인 버튼 숨기기
    
    * til 인증 ui 수정 / til 인증 생성 api 연동
    
    * 프로필 수정 api 연동
    
    * 인증 상세화면
    
    * til 목록 수정 / til 삭제
    
    Co-authored-by: SANGIL PARK <europani1212@gmail.com>
    
    Co-authored-by: SANGIL PARK <europani1212@gmail.com>

--- src/screens/TIL/Detail/Detail.js
+++ src/screens/TIL/Detail/Detail.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import styled from "styled-components";
 import { DetailHome } from "./DetailHome";
 import { Certification } from "./Certification";
+import { CrtDetail } from "./CrtDetail";
 
 function Detail(props) {
   return (
     <Routes>
       <Route path="/" exact element={<DetailHome />} />
       <Route path="/certification" exact element={<Certification />} />
+      <Route path="/:crtid/*" exact element={<CrtDetail />} />
     </Routes>
   );
 }
