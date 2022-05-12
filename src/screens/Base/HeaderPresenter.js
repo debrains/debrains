31ee68baa7cc661b87e2d9796158d06eac0a2220
jangji96 <xxwlgns@naver.com>
2022-05-12 14:17:37 +0900

    로그인버튼 2차수정

--- src/screens/Base/HeaderPresenter.js
+++ src/screens/Base/HeaderPresenter.js
@@ -20,8 +20,9 @@ export default function Example() {
     if (moment(expireAT).diff(moment()) < 0) {
       setIsLogin(false);
     }
+  } else {
+    setIsLogin(false);
   }
-
   const isLogin = useRecoilValue(isLoginAtom);
   return (
     <div className="relative bg-gray-80">
@@ -179,7 +180,7 @@ export default function Example() {
               </Link>
             </Popover.Group>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              {isLogin ? (
+              {!isLogin ? (
                 <>
                   <Link
                     to="/login"
@@ -376,20 +377,29 @@ export default function Example() {
               <div className="py-6 px-5 space-y-6">
                 <div>
                   <p className="text-center text-base font-medium text-gray-500">
-                    <Link
-                      to="/login"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-purple-700 hover:bg-purple-700"
-                    >
-                      로그인
-                    </Link>
-                  </p>
-                  <p className="mt-6 text-center text-base font-medium text-gray-500">
-                    <Link
-                      to="/login"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-purple-700 hover:bg-purple-700"
-                    >
-                      회원가입
-                    </Link>
+                    {!isLogin ? (
+                      <>
+                        <Link
+                          to="/login"
+                          className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+                        >
+                          로그인
+                        </Link>
+                        <Link
+                          to="/login"
+                          className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-purple-700 hover:bg-purple-700"
+                        >
+                          회원가입
+                        </Link>
+                      </>
+                    ) : (
+                      <Link
+                        to="/mypage"
+                        className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+                      >
+                        마이페이지
+                      </Link>
+                    )}
                   </p>
                 </div>
               </div>
