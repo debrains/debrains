bec8fc71e4b1afdba3d905293fb454dd93ef0794
jangji96 <xxwlgns@naver.com>
2022-05-15 17:40:07 +0900

    수정

--- src/screens/Home/HomePresenter.js
+++ src/screens/Home/HomePresenter.js
@@ -8,7 +8,7 @@ function HomePresenter(props) {
         <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">차곡차곡 쌓이는 </span>{" "}
+              <span className="block xl:inline">차곡차곡 쌓이는</span>{" "}
               <span className="block text-purple-600 xl:inline">
                 나의 성장기록
               </span>
