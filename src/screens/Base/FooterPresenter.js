a2c4ac4c9d39a774e7c23cca4cc4d224a714eb16
jangji96 <xxwlgns@naver.com>
2022-05-15 16:37:58 +0900

    디자인 수정

--- src/screens/Base/FooterPresenter.js
+++ src/screens/Base/FooterPresenter.js
@@ -91,15 +91,15 @@ export default function Example() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
-            <Link
+            <a
               key={item.name}
-              to={item.href}
+              href={item.href}
               target={item.target}
               className="text-gray-400 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
-            </Link>
+            </a>
           ))}
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
